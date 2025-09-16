7b40ca9c1db597f497b6b58d4556c124e025dc1d
gerardoezk <gerardo.ezk.080204@gmail.com>
2025-09-15 21:40:06 -0500

    networks.tf

--- networks.tf
+++ networks.tf
@@ -1,16 +1,23 @@
-// Capa de Aplicacion
+/*
+  networks.tf
+  Propósito: definir las redes Docker necesarias para aislar las distintas capas
+  de la infraestructura (aplicación, persistencia y monitoreo).
+  Cada red utiliza el driver "bridge" para la comunicación entre contenedores.
+*/
+
+# Red para la capa de aplicación (servicios web, Nginx, etc.)
 resource "docker_network" "app_net" {
-  name   = "app_net"
-  driver = "bridge"
+  name   = "app_net"  # nombre de la red en Docker
+  driver = "bridge"   # tipo de red
 }
 
-// Capa de Persistencia
+# Red para la capa de persistencia (bases de datos, almacenamiento)
 resource "docker_network" "persistence_net" {
   name   = "persistence_net"
   driver = "bridge"
 }
 
-// Capa de Monitoreo
+# Red para la capa de monitoreo (Grafana, Prometheus, etc.)
 resource "docker_network" "monitor_net" {
   name   = "monitor_net"
   driver = "bridge"
