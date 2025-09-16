975bdcc7f82abda5f2caf46a7f96824a06ba8895
PALS389 <palspiero123@gmail.com>
2025-09-15 22:08:51 -0500

    Update redis.tf

--- redis.tf
+++ redis.tf
@@ -1,20 +1,30 @@
+# Recurso que descarga la imagen oficial de Redis versión 7.4.1 en su variante ligera "alpine"
 resource "docker_image" "redis" {
   name = "redis:7.4.1-alpine"
 }
 
+# Recurso que crea y configura un contenedor Docker con Redis
 resource "docker_container" "redis" {
-  name = "redis-${terraform.workspace}"
-  image = docker_image.redis.image_id
 
-  networks_advanced {
+# El nombre del contenedor incluye el workspace de Terraform (ej: dev, qa, prod)  
+name = "redis-${terraform.workspace}"
+
+# Se usa la imagen definida arriba
+image = docker_image.redis.image_id
+
+# Conexión del contenedor a la red de persistencia 
+networks_advanced {
     name = docker_network.persistence_net.name
   }
 
+# Expone el puerto 6379 (interno de Redis) hacia el mismo puerto en el host
   ports {
     internal = 6379
     external = 6379
   }
 
-  restart = "always"
+# Asegura que el contenedor se reinicie automáticamente en caso de error o reinicio del sistema  
+restart = "always"
   
+
 }
