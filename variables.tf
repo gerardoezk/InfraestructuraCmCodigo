109ae8bf416a8e5fc0ae105431246b533a9cfee4
PALS389 <palspiero123@gmail.com>
2025-09-15 21:59:09 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,12 @@
+# Variable que define la cantidad de contenedores Nginx a crear.
+# Su valor se asigna en el archivo terraform.tfvars.
 variable "nginx_container_count" {
-  type = number
+  type = number   # El valor debe ser un número (ej: 3)
 }
 
+# Variable que define el puerto base desde el cual se expondrán los contenedores.
+# También se asigna en terraform.tfvars.
 variable "nginx_base_port" {
-  type = number
+  type = number   # El valor debe ser un número (ej: 8080)
+
 }
